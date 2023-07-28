2fff91c748f3e41b756a75fd6c6a1718542bdde7
Peter Okorafor <peterdtitan@gmail.com>
2023-07-28 15:29:05 +0300

    Setup tailwind and folder structure

---
+++ client/src/data/dummy.js
@@ -0,0 +1,38 @@
+const homes = [
+    {
+        id: 1,
+        title: 'Kaya Palazzo',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://www.islands.com/wp-content/uploads/sites/13/2022/09/1-mykonos-pool-house.jpg',
+    },
+    {
+        id: 2,
+        title: 'Aqua Fantasy',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://a0.muscache.com/im/pictures/65ad6dc8-9ecf-449f-8c22-5b4993d44538.jpg?im_w=720',
+    },
+    {
+        id: 3,
+        title: 'Kaya Artemis',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://a0.muscache.com/im/pictures/4595e1b4-1f7a-4cf5-9f3d-c84075a00beb.jpg?im_w=720',
+    },
+    {
+        id: 4,
+        title: 'Lares Villa',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://a0.muscache.com/im/pictures/2849368a-4257-4f0a-8c7e-19342e2c8905.jpg',
+    },
+    {
+        id: 5,
+        title: 'Welsh Paradise',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://i2-prod.walesonline.co.uk/incoming/article20171950.ece/ALTERNATES/s1200b/0_JRR_MAI_150321AIRBNB21MOSTPOPULAR_-__05JPG.jpg',
+    },
+    {
+        id: 6,
+        title: 'The Glass Treehouse',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod semper. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.',
+        image: 'https://a0.muscache.com/im/pictures/miso/Hosting-704388413373741312/original/2c632059-f454-4cb3-920d-4510e314e31e.jpeg?im_w=720',
+    },
+];
