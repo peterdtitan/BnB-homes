8f97317af940e9eac35a9a9971301c0722f340de
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-07-28 23:57:32 +0530

    Update routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  get 'current_user/index'
+  get '/current_user' , to: 'current_user#index'
   devise_for :users
-  
-  # root 'pages#index'
+
+  devise_for :users, path: '/', path_names:
+  {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+}
+
 
   namespace :api do
     namespace :v1 do
