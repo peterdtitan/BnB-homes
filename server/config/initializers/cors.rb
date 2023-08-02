12eac49c6717dc46b2de8fcc74c50e0090c21a8c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-02 15:43:11 +0530

    Add delete method in CORS

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,9 +1,6 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :options]
-    end
+  allow do
+    origins 'http://localhost:3001'
+    resource '*', headers: :any, methods: [:get, :post, :options, :delete]
   end
-  
+end
