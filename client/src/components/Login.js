626365002af3a77b9d8cecd96c366423c1706547
Peter Okorafor <peterdtitan@gmail.com>
2023-08-06 09:58:50 +0300

    Create registration and login pages without logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,43 +1,38 @@
 import React from 'react';
 import {Input} from "@nextui-org/react";
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
+import backgroundImage from '../img/login-splash.jpg'; 
 
-export default function () {
-    const [isVisible, setIsVisible] = React.useState(false);
-    const toggleVisibility = () => setIsVisible(!isVisible);
+export default function Login() {
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-4xl font-bold tracking-widest mt-12'>
-            LOGIN TO BnB
-        </h1>
-        <form className='flex flex-col gap-y-10 min-w-[80%]'>
-            <div className='mt-8'>
+    <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className='flex flex-col gap-8 items-center justify-center backdrop-blur-sm h-screen'>
+            <h1 className='text-4xl text-white bg-black/80 p-4 rounded-xl font-bold tracking-widest mt-18'>
+            LOGIN TO BnB HOMES
+            </h1>
+            <form className='flex flex-col gap-6 items-center justify-center rounded-md p-8 bg-slate-200/80 w-[50%]'>
+            <div className=''>
                 <Input
                     isClearable
-                    type="email"
-                    label="Email"
+                    type="text"
+                    label="Username"
                     variant="bordered"
-                    placeholder="Enter your email"
-                    defaultValue="junior@nextui.org"
-                    onClear={() => console.log("input cleared")}
-                    className=""
+                    placeholder="Enter your username"
                 />
             </div>
             <div>
                 <Input
                     isClearable
-                    type="email"
-                    label="Email"
+                    type="password"
+                    label="Password"
                     variant="bordered"
-                    placeholder="Enter your email"
-                    defaultValue="junior@nextui.org"
-                    onClear={() => console.log("input cleared")}
-                    className="max-w-xs"
+                    placeholder="Enter your password"
                 />
             </div>
-        </form>
+            <button className='px-6 py-1 rounded-md bg-green-600 hover:bg-green-900 text-white'>Login</button>
+            </form>
+        </div>
     </div>
   )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {Input} from "@nextui-org/react";
+import { useDispatch, useSelector } from 'react-redux';
+import backgroundImage from '../img/register-splash.jpg'; 
 
 export default function Register() {
+
   return (
-    <div>Register</div>
+    <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className='flex flex-col gap-8 items-center justify-center backdrop-blur-sm h-screen'>
+            <h1 className='text-4xl text-white bg-black/80 p-4 rounded-xl font-bold tracking-widest mt-18'>
+            JOIN BnB HOMES
+            </h1>
+            <form className='flex flex-col gap-6 items-center justify-center rounded-md p-8 bg-slate-200/80 w-[50%]'>
+            <div className=''>
+                <Input
+                    isClearable
+                    type="text"
+                    label="Username"
+                    variant="bordered"
+                    placeholder="Enter new username"
+                />
+            </div>
+            <div>
+                <Input
+                    isClearable
+                    type="password"
+                    label="Password"
+                    variant="bordered"
+                    placeholder="Create your password"
+                />
+            </div>
+            <button className='px-6 py-1 rounded-md bg-green-600 hover:bg-green-900 text-white'>Login</button>
+            </form>
+        </div>
+    </div>
   )
 }
