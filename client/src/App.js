edf9acd258272c95119ca69099f36c6299bdcf69
Peter Okorafor <peterdtitan@gmail.com>
2023-08-06 09:34:00 +0300

    Modify layout logic for login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import Layout from "./components/Layout";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { fetchAllHomes } from "./redux/homesSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Reserve from "./components/Reserve";
 import Reservations from "./components/Reservations";
@@ -12,8 +12,10 @@ import HomeDetails from "./components/HomeDetails";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
+
 function App() {
   const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
     // Fetch all homes when the component mounts
@@ -22,26 +24,37 @@ function App() {
 
   const user = useSelector((state) => state.user.user);
 
+  const isAuthPage = location.pathname === "/Login" || location.pathname === "/Register";
+
   if (user) {
+    // If user data is loading, you can show a loading indicator or any other content
     return <div>Loading...</div>;
-  } else {
+  }
+
+  if (isAuthPage) {
+    // If the current path is "/Login" or "/Register", render the respective component without the Layout
     return (
-      <Layout>
-        <div className="content">
-          <Routes>
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Home/:homeId" element={<HomeDetails />} />
-            <Route path="/Reserve" element={<Reserve />} />
-            <Route path="/Reservations" element={<Reservations />} />
-            <Route path="/AddHome" element={<AddHome />} />
-            <Route path="/RemoveHome" element={<RemoveHome />} />
-          </Routes>
-        </div>
-      </Layout>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     );
   }
+
+  return (
+    <Layout>
+      <div className="content">
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Home/:homeId" element={<HomeDetails />} />
+          <Route path="/Reserve" element={<Reserve />} />
+          <Route path="/Reservations" element={<Reservations />} />
+          <Route path="/AddHome" element={<AddHome />} />
+          <Route path="/RemoveHome" element={<RemoveHome />} />
+        </Routes>
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import {Input} from "@nextui-org/react";
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function () {
+    const [isVisible, setIsVisible] = React.useState(false);
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
     <div className='flex flex-col items-center justify-center'>
         <h1 className='text-4xl font-bold tracking-widest mt-12'>
             LOGIN TO BnB
         </h1>
+        <form className='flex flex-col gap-y-10 min-w-[80%]'>
+            <div className='mt-8'>
+                <Input
+                    isClearable
+                    type="email"
+                    label="Email"
+                    variant="bordered"
+                    placeholder="Enter your email"
+                    defaultValue="junior@nextui.org"
+                    onClear={() => console.log("input cleared")}
+                    className=""
+                />
+            </div>
+            <div>
+                <Input
+                    isClearable
+                    type="email"
+                    label="Email"
+                    variant="bordered"
+                    placeholder="Enter your email"
+                    defaultValue="junior@nextui.org"
+                    onClear={() => console.log("input cleared")}
+                    className="max-w-xs"
+                />
+            </div>
+        </form>
     </div>
   )
 }
