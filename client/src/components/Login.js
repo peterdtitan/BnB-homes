01e3314220f5a783a0faca5b27626268f581c6f7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-09 01:39:52 +0530

    Update login logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,58 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import {Input} from "@nextui-org/react";
-import { useDispatch, useSelector } from 'react-redux';
+import { Input } from "@nextui-org/react";
+import { useDispatch } from 'react-redux'; // Remove the useSelector import
 import { useNavigate } from "react-router-dom";
-import backgroundImage from '../img/login-splash.jpg'; 
+import backgroundImage from '../img/login-splash.jpg';
 import { setUser } from '../redux/user/userSlice'
 
 export default function Login() {
-
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user.user);
+    const dispatch = useDispatch(); // Use useDispatch directly
 
-    const login = async (e) => {
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('user'));
+        if (storedUser && storedUser.username === username && storedUser.password === password) {
+            dispatch(setUser(storedUser));
+            navigate('/');
+        }
+    }, [username, password, dispatch, navigate]);
+
+    const login = (e) => {
         e.preventDefault();
         if (username && password) {
-          dispatch(setUser({ username: username, password: password }))
-          localStorage.setItem('user', JSON.stringify({ username, password }));
-          navigate('/');
+            localStorage.setItem('user', JSON.stringify({ username, password }));
+            dispatch(setUser({ username: username, password: password }));
+            navigate('/');
         } else {
-          console.log('Please enter a valid username and password.');
+            console.log('Please enter a valid username and password.');
         }
     };
 
-  return (
-    <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className='flex flex-col gap-8 items-center justify-center backdrop-blur-sm h-screen'>
-            <h1 className='text-4xl text-white bg-black/80 p-4 rounded-xl font-bold tracking-widest mt-18'>
-            LOGIN TO BnB HOMES
-            </h1>
-            <form className='flex flex-col gap-6 items-center justify-center rounded-md p-8 bg-slate-200/80 w-[50%]'>
-            <div className=''>
-                <Input
-                    type="text"
-                    label="Username"
-                    variant="bordered"
-                    placeholder="Enter your username"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
+    return (
+        <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <div className='flex flex-col gap-8 items-center justify-center backdrop-blur-sm h-screen'>
+                <h1 className='text-4xl text-white bg-black/80 p-4 rounded-xl font-bold tracking-widest mt-18'>
+                    LOGIN TO BnB HOMES
+                </h1>
+                <form className='flex flex-col gap-6 items-center justify-center rounded-md p-8 bg-slate-200/80 w-[50%]'>
+                    <div className=''>
+                        <Input
+                            type="text"
+                            label="Username"
+                            variant="bordered"
+                            placeholder="Enter your username"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            type="password"
+                            label="Password"
+                            variant="bordered"
+                            placeholder="Enter your password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button onClick={login} className='px-6 py-1 rounded-md bg-green-600 hover:bg-green-900 text-white'>Login</button>
+                </form>
             </div>
-            <div>
-                <Input
-                    type="password"
-                    label="Password"
-                    variant="bordered"
-                    placeholder="Enter your password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <button onClick={(e)=>login(e)} className='px-6 py-1 rounded-md bg-green-600 hover:bg-green-900 text-white'>Login</button>
-            </form>
         </div>
-    </div>
-  )
+    )
 }
