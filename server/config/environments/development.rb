3fe38df3c97da6fb925e629f1a6cb4b6c0d95a3b
karan jain <karanjain2212@gmail.com>
2023-08-09 23:31:45 +0530

    Fix rubocop errors phase 1

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -4,15 +4,11 @@ Rails.application.configure do
  
   config.cache_classes = false
 
- 
   config.eager_load = false
-
 
   config.consider_all_requests_local = true
 
-
   config.server_timing = true
-
 
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -27,30 +23,22 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
  
   config.active_storage.service = :local
-
 
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
-
   config.active_support.deprecation = :log
-
 
   config.active_support.disallowed_deprecation = :raise
 
-
   config.active_support.disallowed_deprecation_warnings = []
-
  
   config.active_record.migration_error = :page_load
 
-
   config.active_record.verbose_query_logs = true
-
 
   config.assets.quiet = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
