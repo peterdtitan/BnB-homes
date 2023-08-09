5269cc7c15bbc220fe6b15b94269096b10092b03
karan jain <karanjain2212@gmail.com>
2023-08-09 16:51:50 +0530

    linters

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
+  babelConfig: ".babelrc", // Use the path to your .babelrc file
 };
