0afe017c85bb57f1a5686ddac5180f2b848d98db
Karan Jain <karanjain2212@gmail.com>
2023-08-09 21:35:00 +0530

    Update development.rb

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -13,8 +13,7 @@ Rails.application.configure do
 
   config.server_timing = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
+
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
@@ -29,18 +28,18 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+ 
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
+
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
-  # Print deprecation notices to the Rails logger.
+
   config.active_support.deprecation = :log
 
-  # Raise exceptions for disallowed deprecations.
+
   config.active_support.disallowed_deprecation = :raise
 
   # Tell Active Support which deprecation messages to disallow.
