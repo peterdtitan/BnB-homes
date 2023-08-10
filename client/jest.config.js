df807e2641265c2241274ddacdb65618cb220260
karan jain <karanjain2212@gmail.com>
2023-08-10 20:16:03 +0530

    fixd tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  // preset: 'ts-jest',
+  testEnvironment: "node",
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(axios)/)',
-  ],
+  transformIgnorePatterns: ["/node_modules/(?!(axios)/)"],
 };
