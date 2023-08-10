62a0786397841b067561217768b20211534fca28
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-10 10:22:43 +0530

    Update controller and model

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,27 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    puts "Received parameters: #{params.inspect}"
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 201
+    else
+      render json: {
+        error: 'Error creating user ...'
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
 
   has_many :reservations
 
-  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :username, presence: true, length: { minimum: 8}
+  validates :password, presence: true, length: { minimum: 8 }
 end
