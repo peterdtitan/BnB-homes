0fac52bb347feeaabab660874e3e809023059bdb
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-08-10 18:56:25 +0530

    FIX:test for user slice in frontend

--- client/src/test/userSlice.test.js
+++ client/src/test/userSlice.test.js
@@ -1,4 +1,22 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import userReducer, { setUser, clearUser } from '../redux/user/userSlice';
+import { JSDOM } from 'jsdom';
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = jsdom.window;
+global.document = window.document;
+
+// Configure Enzyme
+configure({ adapter: new Adapter() });
+
+// Initialize the LocalStorageMock
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(), // Add this line
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
 
 describe('userSlice reducers', () => {
   it('should handle setUser', () => {
@@ -22,12 +40,12 @@ describe('userSlice reducers', () => {
       isLoading: false,
       error: null,
     };
-
+  
     const action = clearUser();
-
+  
     const nextState = userReducer(initialState, action);
-
-    expect(nextState.user).toEqual({});
+  
+    expect(nextState.user).toBeNull();
     expect(nextState.isLoading).toBe(false);
   });
 });
