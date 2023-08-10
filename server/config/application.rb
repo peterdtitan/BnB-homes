d6657a205431904534daebe2d264b0c9965da5d2
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-10 13:59:33 +0530

    Fix linters

--- server/config/application.rb
+++ server/config/application.rb
@@ -13,7 +13,7 @@ module Backend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+        resource '*', headers: :any, methods: %i[:get, :post, :put, :patch, :delete, :options]
       end
     end
   end
