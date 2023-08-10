f52de8602eeb6b52197d657c1d1ee9b9dcb68c52
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-08-10 13:45:19 +0530

    ADD:logout spec docs

--- server/spec/requests/api/login_spec.rb
+++ server/spec/requests/api/login_spec.rb
@@ -78,6 +78,39 @@ RSpec.describe Users::SessionsController, type: :request do
       end
     end
   end
+  path '/users/logout' do
+    delete 'User Sign Out' do
+      tags 'Users'
+      produces 'application/json'
+      security [bearer_auth: []]
 
-  
+      response '200', 'Logged out successfully' do
+        schema type: :object,
+               properties: {
+                 status: { type: :integer },
+                 message: { type: :string }
+               },
+               required: %w[status message]
+
+        run_test! do
+          # Create a sample user for testing
+          User.create!(name: 'John Doe', password: 'password')
+
+          # Authenticate the user
+          post '/users/login', params: { user: { name: 'john@example.com', password: 'password' } }
+          auth_header = response.headers['Authorization']
+
+          # Make a request to sign out
+          delete '/users/logout', headers: { Authorization: auth_header }
+
+          # Assert the response status code
+          expect(response).to have_http_status(:ok)
+
+          # Assert the response body against the defined schema
+          json_response = JSON.parse(response.body)
+          expect(json_response).to include('status', 'message')
+        end
+      end
+    end
+  end
 end
