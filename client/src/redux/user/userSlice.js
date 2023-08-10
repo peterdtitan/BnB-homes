30a55b43208b428f5c1a8fe321aa33422ae6e48a
Peter Okorafor <peterdtitan@gmail.com>
2023-08-10 15:22:28 +0300

    Update userSlice

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -10,7 +10,7 @@ const initialState = {
 export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
   try {
     const response = await axios.get('http://127.0.0.1:3000/api/v1/users');
-    return response;
+    return response.data;
   } catch (error) {
     throw error;
   }
