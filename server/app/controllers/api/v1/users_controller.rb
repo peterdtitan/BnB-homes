f99df1a3ad3d0dd63d9aad26e0eee9ad8943de72
Peter Okorafor <peterdtitan@gmail.com>
2023-08-10 08:55:54 +0300

    Update users logic

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -24,6 +24,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:name, :password)
   end
 end
