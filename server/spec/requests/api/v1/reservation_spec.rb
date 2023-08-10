246ddcaf5acd23f05a4efeddca823ee408319454
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-08-10 12:48:06 +0530

    ADD:user to api doc for post

--- server/spec/requests/api/v1/reservation_spec.rb
+++ server/spec/requests/api/v1/reservation_spec.rb
@@ -56,10 +56,11 @@ RSpec.describe Api::V1::ReservationsController, type: :request do
           id: { type: :integer },
           city_id: { type: :integer },
           home_id: { type: :integer },
+          user_id: {type: :integer},
           start_date: { type: :string, format: 'date-time' },
           end_date: { type: :string, format: 'date-time' }
         },
-        required: %w[id city_id home_id start_date end_date]
+        required: %w[id city_id home_id user_id start_date end_date]
       }
 
       response '201', 'Reservation created' do
@@ -68,15 +69,17 @@ RSpec.describe Api::V1::ReservationsController, type: :request do
                  id: { type: :integer },
                  city_id: { type: :integer },
                  home_id: { type: :integer },
+                 user_id: {type: :integer},
                  start_date: { type: :string, format: 'date-time' },
                  end_date: { type: :string, format: 'date-time' }
                },
-               required: %w[id city_id home_id start_date end_date]
+               required: %w[id city_id home_id user_id start_date end_date]
 
         let(:reservation) do
           {
             city_id: 1,
             home_id: 1,
+            user_id:1,
             start_date: DateTime.now,
             end_date: DateTime.now + 1.day
           }
