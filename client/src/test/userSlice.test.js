f8a16eed2e696e6d8336d4e07c3ae59fba5d9eaa
karan jain <karanjain2212@gmail.com>
2023-08-11 00:17:01 +0530

    enzym

--- client/src/test/userSlice.test.js
+++ client/src/test/userSlice.test.js
@@ -1,8 +1,8 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import userReducer, { setUser, clearUser } from '../redux/user/userSlice';
-import { JSDOM } from 'jsdom';
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+import { configure } from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
+import userReducer, { setUser, clearUser } from "../redux/user/userSlice";
+import { JSDOM } from "jsdom";
+const jsdom = new JSDOM("<!doctype html><html><body></body></html>");
 global.window = jsdom.window;
 global.document = window.document;
 
@@ -14,37 +14,37 @@ const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
   removeItem: jest.fn(), // Add this line
-  clear: jest.fn()
+  clear: jest.fn(),
 };
 global.localStorage = localStorageMock;
 
-describe('userSlice reducers', () => {
-  it('should handle setUser', () => {
+describe("userSlice reducers", () => {
+  it("should handle setUser", () => {
     const initialState = {
       user: null,
       isLoading: false,
       error: null,
     };
 
-    const action = setUser({ id: 1, name: 'John' });
+    const action = setUser({ id: 1, name: "John" });
 
     const nextState = userReducer(initialState, action);
 
-    expect(nextState.user).toEqual({ id: 1, name: 'John' });
+    expect(nextState.user).toEqual({ id: 1, name: "John" });
     expect(nextState.isLoading).toBe(false);
   });
 
-  it('should handle clearUser', () => {
+  it("should handle clearUser", () => {
     const initialState = {
-      user: { id: 1, name: 'John' },
+      user: { id: 1, name: "John" },
       isLoading: false,
       error: null,
     };
-  
+
     const action = clearUser();
-  
+
     const nextState = userReducer(initialState, action);
-  
+
     expect(nextState.user).toBeNull();
     expect(nextState.isLoading).toBe(false);
   });
